b707f875550b878c4488dfa3f0ece4540fe53da1
Carl <112435622+cwong8751@users.noreply.github.com>
2024-06-04 11:23:18 -0500

    section 6 37

--- utip/lib/main.dart
+++ utip/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:utip/widgets/person_counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -100,22 +101,11 @@ class _UTipState extends State<UTip> {
                           'Split',
                           style: theme.textTheme.titleMedium,
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                                color: theme.colorScheme.primary,
-                                onPressed: decrement,
-                                icon: const Icon(Icons.remove)),
-                            Text(
-                              "$_personCount",
-                              style: theme.textTheme.titleMedium,
-                            ),
-                            IconButton(
-                                color: theme.colorScheme.primary,
-                                onPressed: increment,
-                                icon: const Icon(Icons.add))
-                          ],
+                        PersonCounter(
+                          theme: theme,
+                          personCount: _personCount,
+                          onDecrement: decrement,
+                          onIncrement: increment,
                         )
                       ],
                     )
